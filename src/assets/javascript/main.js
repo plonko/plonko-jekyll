add5588b5e81220ab2e440d4d664a45fb99f20ad
Seb <sebastianjthomas@gmail.com>
2016-07-01 17:27:53 +0100

    Tidied up js

--- src/assets/javascript/main.js
+++ src/assets/javascript/main.js
@@ -2,80 +2,43 @@
 
 console.log('Hello world!');
 
-var waypoint = new Waypoint({
-    element: document.getElementById('js-waypoint-nav-trigger'),
-    handler: function() {
-        this.element.classList.toggle('fixed');
+
+var plonko = {
+    waypoints: {
+        init: function () {
+            new Waypoint({
+                element: document.getElementById('js-waypoint-nav-trigger'),
+                handler: function() {
+                    this.element.classList.toggle('fixed');
+                },
+                offset: 133
+            })
+        }
     },
-    offset: 133
-});
-
-
-// Loop over all thumbnails, add click, traverse up, swap src
-// var thumbs = document.getElementsByClassName('post-thumbnail');
-
-// for(var t=0; t<thumbs.length; t++) {
-//     thumbs[t].onclick = function(e){
-//         var postImage = this.parentNode.parentNode.querySelector('.post-image');
-//         postImage.src = this.src;
-//     }
-// }
-
-
-// Loop over galleries, delegate click, traverse down, swap src
-var galleries = document.querySelectorAll('.post-gallery');
-
-// for(var i=0; i<galleries.length; i++) {
-//     galleries[i].addEventListener('click', function(e) {
-//         if(e.target && e.target.className == 'post-thumbnail') {
-//             var postImage = e.currentTarget.querySelector('.post-image');
-//             postImage.src = e.target.src;
-//         }
-//         e.stopPropagation();
-//     });
-// }
-
-for(var i=0; i<galleries.length; i++) {
-    bindClicks(galleries[i]);
+    galleries: {
+        settings: {
+            galleryElems: document.querySelectorAll('.post-gallery')
+        },
+        init: function (){
+            this._loopGalleries();
+        },
+        _loopGalleries: function () {
+            var galleryElems = this.settings.galleryElems;
+            for(var i=0; i<galleryElems.length; i++) {
+                this._bindClicks(galleryElems[i]);
+            }
+        },
+        _bindClicks: function (elem){
+            elem.addEventListener('click', function(e) {
+                if(e.target && e.target.className == 'post-thumbnail') {
+                    var postImage = e.currentTarget.querySelector('.post-image');
+                    postImage.src = e.target.src;
+                }
+                e.stopPropagation();
+            });
+        }
+    }
 }
 
-
-function bindClicks (elem) {
-    elem.addEventListener('click', function(e) {
-        if(e.target && e.target.className == 'post-thumbnail') {
-            var postImage = e.currentTarget.querySelector('.post-image');
-            postImage.src = e.target.src;
-        }
-        e.stopPropagation();
-    });
-};
-
-var plonko = (function() {
-
-    var id= 0;
-
-    var init = function() {
-      // Do some setup stuff
-      console.log("init()");
-    };
-
-    var reset = function() {
-        id = 0;
-    }
-    return {
-        init: init
-    };
-})();
-
-// var Gallery = function (elem) {
-//     this.init
-// };
-
-// $('.post-gallery').each(function(){
-//     var thumbs = $(this).find('thumbs');
-// });
-
-// $('.post-thumbnail').click(function(){
-//     var mainImage = $(this).parent().find('.post-image');
-//     mainImage.attr(src, this.src)
-// });
+plonko.waypoints.init();
+plonko.galleries.init();
